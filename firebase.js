f2e04a22ad323c72cc2e39c60ed3760346f0e61d
kidmarvel <morayles2@gmail.com>
2025-07-17 18:28:48 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-lite.js";
 
- // Your web app's Firebase configuration
+// Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyC7nXO2cW3f8Wmm0vyB4Rpehl8-hh4E9yM",
     authDomain: "ebh-report.firebaseapp.com",
@@ -12,9 +17,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
     appId: "1:458081825735:web:b56f8545f707d72d66dfe2"
   };
 
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-export { auth, provider, db, signInWithPopup };
+export { 
+  auth, 
+  db, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut
+};
